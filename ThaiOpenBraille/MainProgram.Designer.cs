6f64fc4436a603b37cadf08adcb04aa13dce45ad
Bhuridech Sudsee <dekcomgang@hotmail.com>
2017-09-18 22:31:34 +0700

    initial UI

--- ThaiOpenBraille/MainProgram.Designer.cs
+++ ThaiOpenBraille/MainProgram.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ThaiOpenBraille
 {
-    partial class Form1
+    partial class MainProgram
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.help = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Font = new System.Drawing.Font("TH SarabunPSK", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(561, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "คำที่ต้องการแปลง";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Controls.Add(this.outputTextBox);
+            this.groupBox2.Font = new System.Drawing.Font("TH SarabunPSK", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(22, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(561, 162);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "แบรลล์";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(6, 28);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(549, 122);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(6, 29);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(549, 127);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // translateButton
+            // 
+            this.translateButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.translateButton.Font = new System.Drawing.Font("TH SarabunPSK", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateButton.Location = new System.Drawing.Point(614, 34);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(148, 128);
+            this.translateButton.TabIndex = 2;
+            this.translateButton.Text = "แปลง";
+            this.translateButton.UseVisualStyleBackColor = false;
+            // 
+            // help
+            // 
+            this.help.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.help.Font = new System.Drawing.Font("TH SarabunPSK", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.help.Location = new System.Drawing.Point(736, 293);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(37, 43);
+            this.help.TabIndex = 3;
+            this.help.Text = "?";
+            this.help.UseVisualStyleBackColor = false;
+            // 
+            // MainProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(785, 348);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainProgram";
+            this.Text = "ThaiOpenBraille (TOB) : โปรแกรมแปลงภาษาไทย/อังกฤษ - แบรลล์ ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.Button help;
     }
 }
 
--- ThaiOpenBraille/MainProgram.cs
+++ ThaiOpenBraille/MainProgram.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ThaiOpenBraille
 {
-    public partial class Form1 : Form
+    public partial class MainProgram : Form
     {
-        public Form1()
+        public MainProgram()
         {
             InitializeComponent();
         }
