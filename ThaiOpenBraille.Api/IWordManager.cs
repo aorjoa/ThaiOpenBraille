118322eb2e76fd68a4f70db2ba0d4c72988c92cd
Weerayut Hongsa <kusumoto.com@gmail.com>
2017-10-11 03:30:21 +0700

    refactor method to api project.

---
+++ ThaiOpenBraille.Api/IWordManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThaiOpenBraille.Api
+{
+    public interface IWordManager
+    {
+		string Output();
+	}
+}
---
+++ ThaiOpenBraille.Api/ThaiDigitsCompare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThaiOpenBraille.Api
+{
+    public class ThaiDigitsCompare
+    {
+		//Parse Thai Number (Digits)
+		public Dictionary<char, int> numDict = new Dictionary<char, int>() { {'๐',0}, { '๑', 1 }, { '๒', 2 },
+			{ '๓', 3 }, { '๔', 4 }, { '๕', 5 },
+			{'๖',6},{'๗',7},{'๘',8},{'๙',9}};
+	}
+}
